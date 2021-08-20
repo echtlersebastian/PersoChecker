fb9ac009c501b6e1384ea62bb0640dd849051bad
Basti <mail@echtlersebastian.de>
2021-08-20 12:19:19 +0200

    test

---
+++ PersoChecker/UnitTest1.cs
@@ -0,0 +1,36 @@
+using NUnit.Framework;
+using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+namespace PersoChecker
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            Console.WriteLine("Setup");
+        }
+
+        [Test]
+        public void CheckMyPerso()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "https://www17.muenchen.de/Passverfolgung/";
+            IWebElement ausweisnummer = driver.FindElement(By.Name("ifNummer"));
+            ausweisnummer.SendKeys("LF0841ZT47");
+
+
+
+            IWebElement abfragen = driver.FindElement(By.Name("pbAbfragen"));
+            abfragen.Click();
+
+
+
+            IWebElement antwort = driver.FindElement(By.CssSelector("b[style='color: red']"));
+            string a = antwort.Text;
+            Console.WriteLine("Test");
+            Assert.AreEqual(a, "noch nicht zur Abholung bereit.");
+        }
+    }
+}
